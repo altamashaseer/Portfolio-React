16bb84073548180cf9aecc0e68f5f670501277c3
Altamash <altamashaseer9@gmail.com>
2022-10-20 23:55:51 +0530

    Contact Component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+  return (
+    <div className='c-container'>
+      <h3 className="heading p-heading">Contact 💻</h3>
+      <div className="c-options">
+        <div className="left">
+
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
