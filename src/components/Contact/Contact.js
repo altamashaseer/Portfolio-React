7465ba9463ea155d74624db84f7b1d9f6ebadb88
Altamash <altamashaseer9@gmail.com>
2022-10-21 23:54:35 +0530

    Contact Fixes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,19 +2,37 @@ import React from 'react'
 import './Contact.css'
 
 function Contact() {
-  return (
-    <div className='c-container'>
-      <h3 className="heading p-heading">Contact 💻</h3>
-      <div className="c-options">
-        <div className="left">
+    return (
+        <div className='c-container'>
+            <h3 className="heading p-heading">Contact 📱</h3>
+            <div className="c-wrapper">
+                <div className="left">
+                    <div className="c-options">
+                        <div className="email card">
+                            <i class="fa-regular fa-envelope"></i>
+                            <h4>Email</h4>
+                            <a href="mailto:altamashaseer9@gmail.com" target='_blank' rel="noreferrer"><h5>altamashaseer9@gmail.com</h5></a>
+                            <a href="mailto:altamashaseer@gmail.com" target='_blank' rel="noreferrer"><h5>altamashaseer@gmail.com</h5></a>
+                        </div>
+                        <div className="phone card">
+                            <i class="fa-solid fa-phone"></i>
+                            <h4>Phone</h4>
+                            <a href="tel:+919997340479"><h5>+91 9997340479</h5></a>
+                        </div>
+                        <div className="email card">
+                            <i class="fa-brands fa-linkedin"></i>
+                            <h4>LinkedIn</h4>
+                            <a href="https://www.linkedin.com/in/altamash-aseer-746aa3193/" target='_blank' rel="noreferrer"><h5>altamashaseer</h5></a>
+                        </div>
+                    </div>
 
-        </div>
-        <div className="right">
+                    <div className="right">
 
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Contact
