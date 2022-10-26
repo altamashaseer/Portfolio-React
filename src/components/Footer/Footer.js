2ffc580265f70b432df2302992b5d42db77785e7
Altamash <altamashaseer9@gmail.com>
2022-10-26 23:56:27 +0530

    Footer Component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Footer.css'
+// import logo from '../../img/logo.png'
 
 function Footer() {
   return (
     <div className='foot-container'>
-      footer
+      <div className="f-wrapper">
+        <span>Made with ❤️ by Altamash | © 2022</span>
+      </div>
     </div>
   )
 }
