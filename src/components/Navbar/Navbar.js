bdba5061e32dcbf7fcca68e156ba6335bfeb90bc
Altamash <altamashaseer9@gmail.com>
2022-10-28 23:56:46 +0530

    Responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../img/logo.png'
 
 
 function Navbar() {
+  const [Toggle, setToggle] = useState(false)
+  const handleClick = () => {
+    setToggle(!Toggle)
+    console.log(Toggle);
+  }
+
   return (
     <div className='n-wrapper' id='Navbar'>
       <div className="n-left">
-        {/* <span className="n-name">A A</span> */}
         <img src={logo} alt="" />
       </div>
       <div className="n-right ">
@@ -19,9 +24,13 @@ function Navbar() {
             <li>Experience</li>
             <li>Projects</li>
             <li>Contact</li>
+            <button className="button n-button">Resume <i className="fa-solid fa-download"></i></button>
           </ul>
         </div>
-        <button className="button n-button">Resume <i className="fa-solid fa-download"></i></button>
+      </div>
+
+      <div id="mobile" onClick={handleClick}>
+        <i className={`${Toggle ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'}`}></i>
       </div>
     </div>
   )
